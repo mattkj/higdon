936e167eaaa46462738121eef391b5047250e630
Matt Johnson <matt@bamsas.com>
2017-05-03 15:56:16 -0700

    wrap everything in function, indent to 2 spaces

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-window.addEventListener('load', () => {
-    const trainingTable = document.querySelector(".table-training");
-    if (!trainingTable) return;
+function higdon() {
+  const trainingTable = document.querySelector(".table-training");
+  if (!trainingTable) return;
 
-    console.log('Training table found');
-    const newPara = document.createElement("p");
-    const convertLink = document.createElement("a");
-    const insertButtonBefore = trainingTable.nextElementSibling;
+  console.log('Training table found');
+  const newPara = document.createElement("p");
+  const convertLink = document.createElement("a");
+  const insertButtonBefore = trainingTable.nextElementSibling;
 
-    trainingTable.style.marginBottom = '15px';
-    newPara.style.textAlign = 'right';
-    newPara.appendChild(convertLink);
-    convertLink.textContent = "Convert to km";
-    convertLink.setAttribute('href', '');
-    trainingTable.parentNode.insertBefore(newPara, insertButtonBefore);
+  trainingTable.style.marginBottom = '15px';
+  newPara.style.textAlign = 'right';
+  newPara.appendChild(convertLink);
+  convertLink.textContent = "Convert to km";
+  convertLink.setAttribute('href', '');
+  trainingTable.parentNode.insertBefore(newPara, insertButtonBefore);
 
-    const originalContent = trainingTable.innerHTML;
+  const originalContent = trainingTable.innerHTML;
 
-    let toggled = false;
+  let toggled = false;
 
-    function toggleUnits() {
+  function toggleUnits() {
     const tds = Array.from(trainingTable.querySelectorAll("td"));
     if (toggled) {
-        tds.forEach(td => {
-        td.innerHTML = td.innerHTML.replace(/[0-9]*\.?[0-9]+\s(m|mile|miles)\s/,match => {
-            let num = parseFloat(match.slice(0, -3));
-            num = Math.round(num * 1.60934 * 2) / 2;
-            return `${num} km `;
-            }
-        );
+      tds.forEach(td => {
+        td.innerHTML = td.innerHTML.replace(/[0-9]*\.?[0-9]+\s(m|mile|miles)\s/, match => {
+          let num = parseFloat(match.slice(0, -3));
+          num = Math.round(num * 1.60934 * 2) / 2;
+          return `${num} km `;
         });
-        convertLink.textContent = 'Convert to miles';
+      });
+      convertLink.textContent = 'Convert to miles';
     } else {
-        trainingTable.innerHTML = originalContent;
-        convertLink.textContent = 'Convert to km';
+      trainingTable.innerHTML = originalContent;
+      convertLink.textContent = 'Convert to km';
     }
-    }
+  }
 
-    convertLink.addEventListener("click", (e) => {
+  convertLink.addEventListener("click", (e) => {
     e.preventDefault();
     toggled = !toggled;
     toggleUnits();
-    });
-});
+  });
+}
+
+higdon();
